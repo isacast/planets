e148ac228c0796fe537120fa293044ea83c10d2f
Isabel Castanho <ic278@exeter.ac.uk>
2017-09-26 14:24:12 +0100

    Just testing.

---
+++ 01-transfer_RNA-seq_files.sh
@@ -0,0 +1,13 @@
+# Isabel Castanho
+
+# Transfer RNA-seq files
+
+nohup wget -r LINK & # example: nohup wget -r ftp://hgdownload.cse.ucsc.edu/goldenPath/mm10/ &
+
+ps #optional
+
+more nohup.out #optional
+
+# another line
+
+
